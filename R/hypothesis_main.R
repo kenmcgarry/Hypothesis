4703041a9a0761fefe6d6816ebe6ea8f84678c8b
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2019-01-05 14:27:13 +0000

    Add files via upload

---
+++ R/hypothesis_main.R
@@ -0,0 +1,16 @@
+# hypothesis_main.R
+# Hypothesis (theory) revision using Bayesian techniques.
+
+# https://www.semanticscholar.org/paper/Necessity%2C-possibility-and-belief%3A-a-study-of-Evans-Handley/e5bd73cbab1477d95719fc3fc8209759bf3fc1eb?tab=abstract&citingPapersSort=is-influential&citingPapersLimit=10&citingPapersOffset=10&citedPapersSort=is-influential&citedPapersLimit=10&citedPapersOffset=0
+
+# https://cran.r-project.org/web/packages/OneR/vignettes/OneR.html
+# https://www.rdocumentation.org/packages/LaplacesDemon/versions/16.1.0
+# https://cran.r-project.org/web/packages/LaplacesDemon/vignettes/LaplacesDemonTutorial.pdf
+# started 19/5/18
+
+#memory.limit(1010241024*1024) # allocate RAM memory (10 GBs)
+setwd("C:/common_laptop/R-files/hypothesis")  # now point to where the new code lives
+source("hypothesis_functions.R")  # load in the functions required for this work. 
+
+
+
